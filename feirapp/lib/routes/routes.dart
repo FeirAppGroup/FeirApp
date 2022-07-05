03a108a8e1349da9c7cadf4790541d8368bcf5fb
lucas-cruz <lucaas-cruuz@live.com>
2022-07-05 20:34:55 -0300

    add estrutura, colors e font

---
+++ feirapp/lib/routes/routes.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class Routes {
+  static const String splashScreen = '/splash-screen';
+
+  static List<GetPage> routes = [
+    // GetPage(
+    //   name: splashScreen,
+    //   //page: (){},
+    // ),
+  ];
+}
---
+++ feirapp/lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color textStyle = Color(0xFF000000);
+
+  static const Color primaryColor = Color(0xFF01B763);
+
+  static const Color shadowColor = Color(0xFFD3D3D3);
+}
