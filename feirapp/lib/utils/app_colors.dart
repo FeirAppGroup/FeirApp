2afd2d9eea3854b179a53b64cb84580f8d2d16f5
lucas-cruz <lucaas-cruuz@live.com>
2022-07-06 19:59:13 -0300

    tela forgot password pronta

--- feirapp/lib/utils/app_colors.dart
+++ feirapp/lib/utils/app_colors.dart
@@ -4,6 +4,7 @@ class AppColors {
   static const Color textStyle = Color(0xFF000000);
 
   static const Color primaryColor = Color(0xFF01B763);
+  static const Color backgroundIconColor = Color(0xFFEBFAF3);
 
   static const Color shadowColor = Color(0xFFD3D3D3);
 }
