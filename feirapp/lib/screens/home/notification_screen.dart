80cc854b6dbca243a8324e184e9efa5daf0582af
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-08-26 08:06:29 -0300

    Att na tela de notificação

--- feirapp/lib/screens/home/notification_screen.dart
+++ feirapp/lib/screens/home/notification_screen.dart
@@ -47,7 +47,6 @@ class _NotificationScreenState extends State<NotificationScreen> {
                     ],
                   );
                 }),
-            dividerLine,
           ],
         ),
       ),
