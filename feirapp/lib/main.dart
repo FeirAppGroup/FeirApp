a6b46cd7c6901f088b15d08dca084650c9b811e3
Matheus Fidelis <matheusfidelis@yahoo.com>
2022-09-21 15:20:21 -0300

    Tema de cores adicionado

--- feirapp/lib/main.dart
+++ feirapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:feirapp/screens/orders/orders_screen.dart';
+import 'package:feirapp/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'routes/routes.dart';
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Urbanist',
-      ),
+      theme: ThemeData(fontFamily: 'Urbanist', useMaterial3: true, colorScheme: AppColors.lightColorScheme),
+      darkTheme: ThemeData(fontFamily: 'Urbanist', useMaterial3: true, colorScheme: AppColors.darkColorScheme),
+      themeMode: ThemeMode.dark,
       title: 'FeirApp',
       initialRoute: Routes.getSplashScreen(),
       getPages: Routes.routes,
