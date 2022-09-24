f98354c6d24f8379ab12884a3ae0ce2aa0c4cdf9
lucas-cruz <lucaas-cruuz@live.com>
2022-09-24 19:46:02 -0300

    Create app_constants.dart

---
+++ feirapp/lib/utils/app_constants.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+class AppConstants {
+  static const String APP_NAME = 'FeirApp';
+  static const int APP_VERSION = 1;
+
+  static const String BASE_URL = 'https://api-feiraapp.herokuapp.com/api';
+  static const String PRODUCTS_URI = '/produto';
+  static const String PRODUCTS_CATEGORY_URI = '/produto/categoria/';
+  static const String PROPERTY_URI = '/propriedade';
+
+  static const String TOKEN = 'token aplication';
+}
